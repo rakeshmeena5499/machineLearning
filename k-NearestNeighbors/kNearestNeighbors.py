2e16b6e2bfef18a7843f988e3643d8525956c915
RKM <rakeshmeena5499@gmail.com>
2021-07-23 18:14:39 +0530

    initial commit

---
+++ k-NearestNeighbors/kNearestNeighbors.py
@@ -0,0 +1,57 @@
+from movies import movie_dataset, movie_labels
+
+def distance(movie1, movie2):
+  dist = 0
+  for i in range(len(movie1)):
+    dist += (movie1[i]-movie2[i])** 2
+  dist = dist ** 0.5
+  return dist
+
+
+def classify(unknown, dataset, labels, k):
+  distances = []
+  #Looping through all points in the dataset
+  for title in dataset:
+    movie = dataset[title]
+    distance_to_point = distance(movie, unknown)
+    #Adding the distance and point associated with that distance
+    distances.append([distance_to_point, title])
+  distances.sort()
+  #Taking only the k closest points
+  neighbors = distances[0:k]
+  num_good = 0
+  num_bad = 0
+  for movie in neighbors:
+    title = movie[1]
+    if(labels[title]==1):
+      num_good+=1
+    else:
+      num_bad+=1
+  return 1 if num_good > num_bad else 0  
+
+print(classify([0.4, 0.2, 0.9], movie_dataset, 5))
+
+
+def find_validation_accuracy(training_set, training_labels, validation_set, validation_labels, k):
+  num_correct = 0.0
+  for movie in validation_set:
+    guess = classify(validation_set[movie], training_set, training_labels, k)
+    if(validation_labels[movie]==guess):
+      num_correct+=1
+  return num_correct/len(validation_set)
+
+print(find_validation_accuracy(training_set, training_labels, validation_set, validation_labels, 4))
+
+
+
+
+#Rather than writing our own classifier every time, we can use Python’s sklearn library.
+
+from movies import movie_dataset, labels
+from sklearn.neighbors import KNeighborsClassifier
+
+classifier = KNeighborsClassifier(n_neighbors=5)
+
+classifier.fit(movie_dataset, labels)
+
+print(classifier.predict([[.45, .2, .5], [.25, .8, .9],[.1, .1, .9]]))
---
+++ k-NearestNeighbors/script.py
@@ -0,0 +1,65 @@
+'''
+#Euclidean Distance
+To find the Euclidean distance between two points, we first calculate the squared distance between each dimension.
+If we add up all of these squared differences and take the square root, we’ve computed the Euclidean distance.
+'''
+
+def euclidean_distance(pt1, pt2):
+  distance = 0
+  for i in range(len(pt1)):
+    distance += (pt1[i]-pt2[i])**2
+  distance = distance**0.5
+  return distance
+
+print(euclidean_distance([5, 4, 3], [1, 7, 9]))
+
+
+'''
+#Manhattan Distance
+Manhattan Distance is extremely similar to Euclidean distance. Rather than summing the squared difference between each
+dimension, we instead sum the absolute value of the difference between each dimension. It’s called Manhattan distance
+because it’s similar to how you might navigate when walking city blocks.
+'''
+
+def manhattan_distance(pt1, pt2):
+  distance = 0
+  for i in range(len(pt1)):
+    distance += abs(pt1[i]-pt2[i])
+  return distance
+
+print(manhattan_distance([1, 2], [4, 0]))
+
+
+'''
+#Hamming Distance
+Hamming distance only cares about whether the dimensions are exactly equal. When finding the Hamming distance between two points,
+add one for every dimension that has different values. Hamming distance is used in spell checking algorithms. For example, 
+the Hamming distance between the word “there” and the typo “thete” is one.
+'''
+
+def hamming_distance(pt1, pt2):
+  distance = 0
+  for i in range(len(pt2)):
+    if(pt1[i]!=pt2[i]):
+      distance+=1
+  return distance
+
+print(hamming_distance([1,2], [1, 100]))
+
+
+
+'''
+#SciPy Distances
+1. Euclidean Distance .euclidean()
+2. Manhattan Distance .cityblock()
+3. Hamming Distance .hamming()
+
+The scipy implementation of Hamming distance will always return a number between 0 an 1, rather than summing the number of differences
+in dimensions, this implementation sums those differences and then divides by the total number of dimensions.
+'''
+
+from scipy.spatial import distance
+
+print(distance.euclidean([1,2],[4,0]))
+print(distance.cityblock([1,2],[4,0]))
+print(distance.hamming([5, 4, 9], [1, 7, 9]))
